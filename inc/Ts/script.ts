ee189ebf1551f3c8303de11307947553445b4484
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2023-04-05 09:46:25 +0200

    Commit 2

--- inc/Ts/script.ts
+++ inc/Ts/script.ts
@@ -1,39 +1,66 @@
-//Barre de progression
+// const button1: HTMLButtonElement = document.getElementById("button_1") as HTMLButtonElement;
+// const hiddenClass1: HTMLDivElement = document.getElementById("hidden_class1") as HTMLDivElement;
+// const hiddenClass2: HTMLDivElement = document.getElementById("hidden_class2") as HTMLDivElement;
 
-const healthBar = document.querySelector('#health_bar');
-const increaseHealthButton = document.querySelector('#increase_health_button');
+// button1.addEventListener("click", () => {
+//   hiddenClass1.classList.add("hidden");
+//   hiddenClass1.classList.remove("display");
 
-increaseHealthButton.onclick = function() {
-  if (healthBar.value < healthBar.max) {
-    healthBar.value++;
-    updateHealthBar();
+//   hiddenClass2.classList.add("display");
+//   hiddenClass2.classList.remove("hidden");
+// });
+
+console.log("hello");
+
+// const button = document.getElementById("button_1");
+// const hiddenClass1 = document.getElementById("hidden_class1");
+// const hiddenClass2 = document.getElementById("hidden_class2");
+
+// button.addEventListener("click", () => {
+//   hiddenClass1.classList.add("hidden");
+//   hiddenClass1.classList.remove("display");
+
+//   hiddenClass2.classList.add("display");
+//   hiddenClass2.classList.remove("hidden");
+// })
+// ;
+
+import test from '../assets/images/butters1-removebg-preview.png'
+
+let perso = {
+  first_button: "./Eric-cartman1-removebg-preview.png",
+  second_button: `${test}`,
+  third_button: "./kenny-removebg-preview.png",
+};
+let bouton = "first_button";
+
+function choisirPersonnage(button) {
+  if (Object.keys(perso).includes(button)) {
+    let perso_image = perso[button];
+    const image:HTMLImageElement = document.createElement("img")
+    image.src = `${test}`;
+
+
+    document.querySelector(".left_column")?.appendChild(image);
   }
+  var elementPersonnage: HTMLElement = document.querySelector(
+    ".personnage" + button
+  )!;
+
+  elementPersonnage.style.border = "2px solid red";
 }
 
-setInterval(function() {
-  if (healthBar.value >= 50) {
-    healthBar.value -= healthBar.max * 0.1;
-    updateHealthBar();
-  } else if (healthBar.value >= 15 && healthBar.value < 50) {
-    healthBar.value -= healthBar.max * 0.06;
-    updateHealthBar();
-  } else {
-    healthBar.value -= healthBar.max * 0.02;
-    updateHealthBar();
-  }
-}, 1000);
+const buttonChoice1 = document.getElementById("first_button")!;
+buttonChoice1.addEventListener("click", function () {
+  choisirPersonnage("first_button");
+});
 
-function updateHealthBar() {
-  let percent = (healthBar.value / healthBar.max) * 100;
+const buttonChoice2 = document.getElementById("second_button")!;
+buttonChoice2.addEventListener("click", function () {
+  choisirPersonnage("second_button");
+});
 
-  if (percent >= 50) {
-    healthBar.style.setProperty('--progress-color', 'green');
-    healthBar.style.setProperty('--progress-value', `${percent}%`);
-  } else if (percent >= 15 && percent < 50) {
-    healthBar.style.setProperty('--progress-color', 'yellow');
-    healthBar.style.setProperty('--progress-value', `${percent}%`);
-  } else {
-    healthBar.style.setProperty('--progress-color', 'red');
-    healthBar.style.setProperty('--progress-value', `${percent}%`);
-  }
-}
+const buttonChoice3 = document.getElementById("third_button")!;
+buttonChoice3.addEventListener("click", function () {
+  choisirPersonnage("third_button");
+});
